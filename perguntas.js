970631af0daa742709227dffec3a6d0c150653a6
LKmourinha <133127745+LKmourinha@users.noreply.github.com>
2024-10-09 23:53:14 +0000

    alterações

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Programação',
+    'O que é programação?',
+    'O python é uma língua de programação'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital da França?',
+    'A capital da frança é Paris'
+)
+
+criaCartao(
+    'Basquete',
+    'Qual time Michael Jordan jogava quando ganhou 6 títulos?',
+    'Michael Jordan jogava pelo Chicago Bulls'
+)
+
+criaCartao(
+    'Futebol Americano',
+    'Quando Patrick Mahomes ganhou seu último título?',
+    'foi em 11/02/2024 com seu time Kansas City Chiefs'
+)
